40bf84ff67fac4723550c777ef1d65aae9a53833
Kumar R U Bharti <kumar@dynascopetechnologies.com>
2025-10-31 15:27:55 +0530

    graphql middleware

--- src/middlewares/graphql.middleware.js
+++ src/middlewares/graphql.middleware.js
@@ -1,25 +1,27 @@
 const jwt = require("jsonwebtoken");
 
 const graphqlMiddleware = (req, res, next) => {
-  if (req.headers.authorization) {
-    const token = req.headers.authorization.split(" ")[1];
+  const authHeader = req.headers.authorization;
 
-    const verifyToken = jwt.verify(token, process.env.JWT_SECRET);
-
-    if (verifyToken) next();
-
-    res.status(400).send({ message: "Bhaag yaha se" });
+  if (!authHeader) {
+    return res.status(401).json({ message: "Authorization header missing" });
   }
 
-  const token =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImQ2NzY2OWE5LTY0MGUtNDhkNy04ZDFmLTJiZDQzMzMxZjNhZiIsInJvbGUiOiJjdXN0b21lciIsImlhdCI6MTc2MTgyMDI3NiwiZXhwIjoxNzYxOTA2Njc2fQ.U1-HauTLjDet9frwkKhPIUMvKDo6SsSsb38XHDVo8MU";
+  const token = authHeader.split(" ")[1];
+  // const token =
+  //   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImQ2NzY2OWE5LTY0MGUtNDhkNy04ZDFmLTJiZDQzMzMxZjNhZiIsInJvbGUiOiJjdXN0b21lciIsImlhdCI6MTc2MTgyMDI3NiwiZXhwIjoxNzYxOTA2Njc2fQ.U1-HauTLjDet9frwkKhPIUMvKDo6SsSsb38XHDVo8MU";
+
+  if (!token) {
+    return res.status(401).json({ message: "Token missing" });
+  }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
   } catch (err) {
-    res.status(401).json({ message: "Token not valid." });
+    console.error("JWT Error:", err.message);
+    return res.status(401).json({ message: "Invalid or expired token" });
   }
 };
 
